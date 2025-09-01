cd749a97daa90ffbcc99ac2b99c0dd5a062b18e3
zakuro0396-ctrl <mu.work0396@gmail.com>
2025-09-01 17:34:30 +0900

    kadai_007追加

---
+++ kadai_007/calc.js
@@ -0,0 +1,17 @@
+let num =15;
+
+if (num%15==0) {
+  console.log('3と5の倍数です')
+}
+
+else if (num%3==0) {
+  console.log('3の倍数です')
+}
+
+else if (num%5==0) {
+  console.log('5の倍数です')
+}
+
+else {
+  console.log (num)
+}
